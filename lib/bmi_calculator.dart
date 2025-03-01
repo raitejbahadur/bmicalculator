76331051408d2835e926c1a95594fc389be5d85c
HP <raitejbahadur786@gmail.com>
2025-03-01 23:36:02 +0530

    Updating comments

--- lib/bmi_calculator.dart
+++ lib/bmi_calculator.dart
@@ -4,6 +4,7 @@ import 'package:bmi_calculator/body_model.dart';
 
 // I've used weight/height^2 (kg/m^2)
 // You can expand this logic
+// TejBahadur - Ok. Need to review.
 double calculateBMI({required BodyModel bodyModel}) {
   return (bodyModel.weight) / pow(bodyModel.height / 100, 2);
 }
